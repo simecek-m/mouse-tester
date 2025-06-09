c7b0bb04de302ff2024739bd2c7438804bd4e7e1
Martin Šimeček <simecek.m@seznam.cz>
2025-06-09 23:41:31 +0200

    fix relative paths in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import App from "@/App";
+import { ThemeProvider } from "@/providers/ThemeProvider";
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { ThemeProvider } from "./providers/ThemeProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
